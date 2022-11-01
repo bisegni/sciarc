edd2b0ff1ce5ba95df875e59e2074c85326b1a2e
Claudio Bisegni <bisegni@users.noreply.github.com>
2022-11-01 17:09:26 +0000

    fixes

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 /*
-#cgo CFLAGS: -I . -stdlib=libc++
-#cgo LDFLAGS: -L${SRCDIR}/build/depinstall/lib -ldcomp -lstdc++
-#include "engine/src/dcomp.h"
+#cgo CFLAGS: -I .
+#cgo LDFLAGS: -L${SRCDIR}/build/local/lib -ldcomp
+#include "${SRCDIR}/build/local/sciarc/sciarc.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
