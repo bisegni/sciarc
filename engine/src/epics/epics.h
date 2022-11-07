9f5c41d6783fd9150ec8588123a2ba7dfb648c03
Claudio Bisegni <bisegni@users.noreply.github.com>
2022-11-07 02:18:07 +0000

    add osme epics base code for test ws

---
+++ engine/src/epics/epics.h
@@ -0,0 +1,15 @@
+#include <cadef.h>
+#include <string>
+#include <memory>
+#include <pva/client.h>
+class EpicsChannel {
+    const std::string channel_name;
+    pvac::ClientProvider provider;
+    std::unique_ptr<pvac::ClientChannel> channel;
+public:
+explicit EpicsChannel(const std::string& channel_name);
+~EpicsChannel() = default;
+void connect();
+epics::pvData::PVStructure::const_shared_pointer
+get();
+};
